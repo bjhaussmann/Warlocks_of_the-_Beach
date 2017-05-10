b2c27ef15504eb13375f5a8423d57f75fa77a4e4
Daniel Santana Medin <Daniel Santana Medin@DESKTOP-JIBMGJ6.fios-router.home>
2017-05-09 20:22:25 -0700

    Commit

---
+++ MovingObject.java
@@ -0,0 +1,16 @@
+
+
+public class MovingObject {
+
+	private int pXPosition;
+	private int pYPosition;
+	
+	public int getpXPosition() {
+		return pXPosition;
+	}
+	public int getpYPosition() {
+		return pYPosition;
+	}
+	
+	
+}
