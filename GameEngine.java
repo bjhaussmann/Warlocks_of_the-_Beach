614bc1bea669385cc6f0538d41ad0a64b64c61d5
Tom Landry <Tom Landry@10.110.194.234>
2017-05-10 09:26:56 -0700

    Uploaded Game engine Skeleton Class

---
+++ GameEngine.java
@@ -0,0 +1,67 @@
+
+
+public class GameEngine {
+
+	
+	public void mStartGame()
+	{
+		
+	}
+	
+	public void mGenerateGameBoard()
+	{
+		
+	}
+	
+	public void mLoadSave()
+	{
+		
+	}
+	
+	public void mNewGameBoard()
+	{
+		
+	}
+	
+	public void CoreGameLoop()
+	{
+		
+	}
+	
+	public void mQuit()
+	{
+		
+	}
+	
+	public void mSave()
+	{
+		
+	}
+	
+	public void mLook()
+	{
+		
+	}
+	
+	public void mShoot()
+	{
+		
+	}
+	
+	public void mPMove()
+	{
+		
+	}
+	
+	public void mNMove()
+	{
+		
+	}
+	
+	public void mCheckGameState()
+	{
+		
+	}
+	
+	
+}
