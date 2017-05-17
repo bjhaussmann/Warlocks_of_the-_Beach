21b6dd8b99e9b8ebc3672ef61a665968ac5fe521
Cesar <Cesar@10.110.116.225>
2017-05-17 10:19:00 -0700

    updated game space

--- edu/cpp/cs/cs141/WarlocksOfTheBeach/GameSpace.java
+++ edu/cpp/cs/cs141/WarlocksOfTheBeach/GameSpace.java
@@ -1,20 +1,36 @@
 package edu.cpp.cs.cs141.WarlocksOfTheBeach;
-
-/**
- * 
- */
-
 /**
  * @author bjhau
- *
+ *	author Cesar
+ *	GameSpace is a parent object of the four types of items. They will have the mReturnType method to return
+ *	its String name. The children objects call upon the super constructor to assign the appropriate String name.
  */
 public class GameSpace {
 
-	private String pName;
+	final private String pName;
 	
+	private int pXPosition;
 	
-	public String ReturnType () {
+	private int pYPosition;
+	
+	public GameSpace(String tName) {
+		this.pName = tName;
+	}
+	
+	public String mReturnType () {
 		return pName;
 	}
 	
+	public void setXPosition(int tXPosition) {
+		this.pXPosition = tXPosition;
+	}
+	
+	public void setYPosition(int tYPosition) {
+		this.pYPosition = tYPosition;
+	}
+	
+	public int getPosition() {
+		int tPosition = ((this.pYPosition * 9) + this.pXPosition);
+		return tPosition;
+	}
 }
