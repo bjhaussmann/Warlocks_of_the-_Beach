6bc83a37e85297905a8cc9641030a5e3755cd4fa
EChilin5 <edgar3ac@gmail.com>
2017-05-29 17:19:08 -0700

    Update DocumenControl.java

--- edu/cpp/cs/cs141/WarlocksOfTheBeach/DocumenControl.java
+++ edu/cpp/cs/cs141/WarlocksOfTheBeach/DocumenControl.java
@@ -22,45 +22,69 @@ import java.util.Scanner;
  * @author bjhau
  *
  */
-public class DocumenControl implements Serializable {
-	
-	
+public class DocumenControl {
+	private Player cPlayer;
+	private Ninja cNinja;
+	private GameEngine cGameBoard;
+	ArrayList<GameEngine> zGameboard = new ArrayList<GameEngine>();
+	ArrayList<Player> zPlayer = new ArrayList<Player>();
+	ArrayList<Ninja> zNinja = new ArrayList<Ninja>();
 
-	
-	private ClassLoader mGetGameBoard(){
-		return ;
-		}
-	private void mGetPlayer(){
-		return player.
-	}
-	private void mGetNinja(){
+//	GameEngine[] gBoard = new GameEngine[10];
+//	Player[] gPlayer = new Player[10];
+//	Ninja[] gNinja = new Ninja[10];
+
+	private void currentData() {
+		inputGameBoard();
+		inputPlayer();
+		inputNinja();
+//		Player[] gPlayer = new Player[10];
 		
 	}
-	private void mGetFiles(){
-		
+
+	/**
+	 * 
+	 */
+	private void inputGameBoard() {
+
 	}
-	
+	//there error because the paramenter in player are just Player() 
+	private void inputPlayer() {
+		zPlayer.add(new Player(cPlayer.mGetBullets(), cPlayer.mGetInvincibility(), cPlayer.mGetLives(), cPlayer.mIsRadar()));
+	}
+
+	/**
+	 * this will hold all the player current status
+	 */
+	private void inputNinja() {
+		zNinja.add(new Ninja(cNinja.mGetXPosition(), cNinja.mGetYPosition()));
+
+	}
+
+	/**
+	 * this will hold all of the ninjas current status
+	 */
+
 	/**
 	 * 
 	 * @param mSave
 	 *            This will store the current objects into memory so the game
-	 *            could be saved.It will all be stored in a file.
-	 *            Has to follow specific order: mGetGameBoard, mGetPlayer, mGetNinja,
+	 *            could be saved.It will all be stored in a file. Has to follow
+	 *            specific order: mGetGameBoard, mGetPlayer, mGetNinja,
 	 *            mCloseDoc
 	 */
 	public void mSave(File Name) {
-//		ArrayList<ContainObjects> pieces = new ArrayList<ContainObjects>();
-//		ContainObjects gBoard = new ContainObjects(mGetGameBoard());
-//		ContatinObjects gPlayer =  new ConatinObject(mGetPlayer());
-//		ContainObjects gNinja =  new ContainObject(mGetNinja());
-		
+		ArrayList<Object> data = new ArrayList<Object>();
+		data.add(zGameboard);
+		data.add(zPlayer);
+		data.add(zNinja);
+
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FileName));
-			pieces.add(gBoard);
-			pieces.add(gPlayer);
-			pieces.add(gNinja);
-			out.writeObject(pieces);
+			FileOutputStream fileOut = new FileOutputStream("data.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(data);
 			out.close();
+			fileOut.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -72,19 +96,35 @@ public class DocumenControl implements Serializable {
 	 * @param Deserialize
 	 *            This will be used to call a file that'll pull out the
 	 *            different objects from the last occurrence that a player saved
-	 *            the game.
-	 *            return -1 if there an error 
+	 *            the game. return -1 if there an error
 	 */
 	public static void mOpenDoc(File Name) throws FileNotFoundException {
+
+		ArrayList<Object> deserialize = new ArrayList<Object>();
+
 		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FileName));
-			ois.readObject();
+			FileInputStream fileIn = new FileInputStream("data.ser");
+			ObjectInputStream ois = new ObjectInputStream(fileIn);
+			deserialize = (ArrayList<Object>) ois.readObject();
 			ois.close();
-		} catch (IOException e) {
+			fileIn.close();
+		} catch (IOException | ClassNotFoundException e) {
 			System.out.printf("-1", e);
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	public static void mSearchFile() {
+		File file = new File(
+				"/Users/edgarchilin/eclipse/java-neon/Eclipse.app/Contents/MacOS/https:/github.com/bjhaussmann/Warlocks_of_the_Beach.git");
+		String[] str = file.list();
+		for (String string : str) {
+			if (string.endsWith(".ser")) {
+				System.out.println(string);
+			}
+
+		}
 	}
 
 }
