f72613f81e04828187a246ebac646bfe311dd175
Daniel Santana Medin <Daniel Santana Medin@DESKTOP-JIBMGJ6.fios-router.home>
2017-06-07 15:12:48 -0700

    Added comments

--- edu/cpp/cs/cs141/WarlocksOfTheBeach/Ninja.java
+++ edu/cpp/cs/cs141/WarlocksOfTheBeach/Ninja.java
@@ -3,18 +3,30 @@ package edu.cpp.cs.cs141.WarlocksOfTheBeach;
 import java.util.Random;
 import java.io.Serializable;
 /**
- * @author edgarchilin pliving mMove mCheckLinezOfSight mCheckKill
- *         mCheckInvincibility fix set from saved games
+ * The Ninja class computes the kill, death, spawning, and movement. 
+ * It takes an int as the position within the grid to represent the position.
+ * We use this to calculate the kill and spawning.
+ * Also declares its property of health and its own death.
+ * The position of the Ninja is unique using only one int rather than two
  * 
+ * @author Daniel Santana Medina
  */
 public class Ninja extends MovingObject {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -4720248096711044295L;
+	/*
+	 * The ninja's property health
+	 * Used to calculate the death
+	 */
 	private int pHealth;
 
+	/*
+	 * The constructor spawns the ninja in a valid position within the grid 
+	 * We multiply by 9 and add the other position to get the number 0-80 of the grid
+	 * This will be the single position 
+	 * 
+	 * It also gives the ninja's health
+	 */
 	public Ninja() {
 		super();
 		do {
@@ -30,28 +42,36 @@ public class Ninja extends MovingObject {
 		pHealth = 1;
 
 	}
-
+	/*
+	 * Uses the single int position of the player which is passed from the game engine to check if it is equal
+	 * to the ninjas. We add/subtract 1 to check right/left and add/subtract 9 to check up/down, respectively. 
+	 */
 	public int mComputeKill(int tPP) {
 		if (((tPP == pPosition - 1) || (tPP == pPosition + 1) || (tPP == pPosition - 9) || (tPP == pPosition - 1)) && pHealth > 0)
 			return 1;
 		else
 			return 0;
 	}
-
+	/*
+	 * The ninja's death method 
+	 * If the player shoots the Ninja down 
+	 */
 	public int mNinjaDeath() {
 		return pHealth--;
 	}
-
+	/*
+	 * We call the moving method in the superclass to compute the movement 
+	 * Passes a 0-3 number and checks if valid else tries again 
+	 * It checks for edges and corners, including rooms
+	 */
 	public void mMoveNinja() {
 		boolean tVM = false;
 		do {
 			int tD = new Random().nextInt(4);
 			int tMR = super.mMove(tD);
 			int tPP = pPosition;
-			// Ninjas will not go into room but jump over instead. fixing
 
-			if (tMR > -1) // Check valid movement if yes check Position not
-							// equal rooms
+			if (tMR > -1) 
 			{
 				if ((pPosition != 10) && (pPosition != 13) && (pPosition != 16) && (pPosition != 37)
 						&& (pPosition != 40) && (pPosition != 43) && (pPosition != 64) && (pPosition != 67)
@@ -65,7 +85,10 @@ public class Ninja extends MovingObject {
 		} while (tVM == false);
 	}
 
-	// Getter for the properties
+	/*
+	 * These return the health and position of the Ninja
+	 * Useful for the game engine 
+	 */
 
 	public int mGetEnemyHealth() {
 		return pHealth;
